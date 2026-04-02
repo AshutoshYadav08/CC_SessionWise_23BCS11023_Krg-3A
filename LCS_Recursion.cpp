b2c2c06d1b720af5010062b5172481fbab9792d3
AshutoshYadav08 <ashu2017ccyadav@gmail.com>
2026-04-02 15:36:37 +0530

    LCS

---
+++ LCS_Recursion.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lcsRec(string &s1, string &s2, int i, int j){
+    if(i < 0 || j < 0) return 0;
+
+    if(s1[i] == s2[j])
+        return 1 + lcsRec(s1, s2, i-1, j-1);
+    else
+        return max(lcsRec(s1, s2, i-1, j),
+                   lcsRec(s1, s2, i, j-1));
+}
+
+int main(){
+    string s1 = "abcde";
+    string s2 = "ace";
+
+    cout << lcsRec(s1, s2, s1.size()-1, s2.size()-1);
+}
